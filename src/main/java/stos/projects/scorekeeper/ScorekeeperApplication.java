68ac82b441ff60e4d2abf905e13851d41de54c89
Stuart Osborne <stuart.osborne@infinera.com>
2018-08-15 16:22:19 +0200

    Initial commit.

---
+++ src/main/java/stos/projects/scorekeeper/ScorekeeperApplication.java
@@ -0,0 +1,12 @@
+package stos.projects.scorekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorekeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScorekeeperApplication.class, args);
+    }
+}
