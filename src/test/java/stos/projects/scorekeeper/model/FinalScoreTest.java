2e2eece15c622086a15ce27ba0cd93b5f34d7107
Stuart Osborne <stuart.osborne@infinera.com>
2018-09-02 12:08:08 +0200

    Fixed failing tests, now that FinalScore is an entity.

--- src/test/java/stos/projects/scorekeeper/model/FinalScoreTest.java
+++ src/test/java/stos/projects/scorekeeper/model/FinalScoreTest.java
@@ -11,23 +11,23 @@ public class FinalScoreTest {
 
     @Test
     public void scores_are_equal() {
-        FinalScore finalScore1 = new FinalScore(1, 1);
-        FinalScore finalScore2 = new FinalScore(1, 1);
+        FinalScore finalScore1 = new FinalScore(1,1, 1);
+        FinalScore finalScore2 = new FinalScore(1,1, 1);
 
-        assertThat(finalScore1, is(equalTo(finalScore2)));
+        assertThat(finalScore1, is((equalTo(finalScore2))));
     }
 
     @Test
     public void scores_are_not_equal() throws Exception {
-        FinalScore finalScore1 = new FinalScore(1, 1);
-        FinalScore finalScore2 = new FinalScore(1, 3);
+        FinalScore finalScore1 = new FinalScore(1,1, 1);
+        FinalScore finalScore2 = new FinalScore(1,1, 3);
 
         assertThat(finalScore1, is(not(equalTo(finalScore2))));
     }
 
     @Test
     public void toString_is_printed_correctly() throws Exception {
-        FinalScore finalScore1 = new FinalScore(1, 7);
+        FinalScore finalScore1 = new FinalScore(1,1, 7);
 
         String expectedString = "[ 1 - 7 ]";
         assertThat(finalScore1.toString(), is(expectedString));
